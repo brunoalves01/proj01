7e5f79bed899bd7380b069109c1edb5f0685ef50
brunoalves01 <brutavinho@gmail.com>
2026-04-01 09:34:26 -0300

    Números pares ou ímpares

---
+++ ex03/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt ("Por gentileza insira o número desejado."));
+
+if (numero % 2 === 0) {
+    alert ("O número inserido é par!");
+
+} else {
+    alert ("O número inserido é ímpar");
+}
+    
