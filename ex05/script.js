e113e388e780e7c0e11df9b09ea1b683a9ad2c9b
brunoalves01 <brutavinho@gmail.com>
2026-04-01 09:59:33 -0300

    Valores múltiplos de 5

---
+++ ex05/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Por gentileza insira o número desejado"));
+if (numero % 5 === 0) {
+    alert ("Que legal, esse número é um múltiplo de 5!");
+} else {
+    alert ("Esse número não é um múltiplo de 5!");
+}
+    
