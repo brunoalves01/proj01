7b9657016d0dd81e8bafea2f0bf08ef21af6a16b
brunoalves01 <brutavinho@gmail.com>
2026-04-01 09:23:34 -0300

    Verificação de idade

--- ex02/script.js
+++ ex02/script.js
@@ -1,7 +1,7 @@
-let idade = Number(prompt ("insira a sua idade"));
+let idade = Number(prompt ("Por gentileza insira a sua idade."));
 
 if (idade <18) {
-    alert ("Você ainda é menor de idade"); 
+    alert ("Você ainda é menor de idade!"); 
 } else {
     alert ("Maior de idade!");
 }
